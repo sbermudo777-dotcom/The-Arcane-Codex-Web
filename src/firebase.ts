61cf7c492b17c53a687a297bb64bd72c400afc5f
samuel <@samuelbermudo777>
2026-05-07 09:47:47 +0200

    feat: initial project setup with Vue 3, Tailwind CSS v4 and Firebase SDK

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6g_OyljnZgmeFjqOvRjd4NtinYjKpOUQ",
+  authDomain: "the-arcane-codex.firebaseapp.com",
+  projectId: "the-arcane-codex",
+  storageBucket: "the-arcane-codex.firebasestorage.app",
+  messagingSenderId: "65959588585",
+  appId: "1:65959588585:web:ff91d4ea1741ae4fc67678"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
